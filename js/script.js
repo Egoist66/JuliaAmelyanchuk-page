e013961c7c4ab2c2267b0f975fb672f87bf7a167
Farid <razormad666@gmail.com>
2022-03-27 15:38:49 +0300

    second fix

---
+++ js/script.js
@@ -0,0 +1,78 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let navigation = document.querySelector('.navigation');
+    let focused_li = document.querySelector('.sub_li');
+    let elem = `
+    <div class="menu-burger">
+    
+        <span></span>
+        <span></span>
+        <span></span>
+    
+    </div>`
+
+    navigation.insertAdjacentHTML('beforeend', elem);
+    let burger = document.querySelector('.menu-burger');
+
+
+    function activeBurger() {
+
+        this.classList.toggle('active');
+    }
+
+    function activeContact(event) {
+
+        let subnav_list = document.querySelector('.subnaviation-list');
+
+        console.log(event.target);
+
+        if (event.target.classList.contains('sub_link')) {
+            subnav_list.classList.toggle('active');
+        } else {
+            subnav_list.classList.remove('active');
+        }
+    }
+
+    function stickyHeader() {
+
+        let head = document.querySelector('.head');
+        let nav_links = document.querySelectorAll('.navigation-list__links ');
+
+        if (window.scrollY > 0) {
+            head.classList.add('sticky')
+            nav_links.forEach(link => {
+                link.classList.add('active');
+            })
+        } else {
+            head.classList.remove('sticky');
+            nav_links.forEach(link => {
+                link.classList.remove('active');
+            })
+        }
+
+
+        if (window.outerWidth <= 1017) {
+
+            head.classList.remove('sticky');
+        }
+    }
+
+
+
+    let sub_link = document.querySelector('.sub_link');
+    sub_link.addEventListener('click', function (event) {
+        event.preventDefault();
+    });
+
+
+    burger.addEventListener('click', activeBurger);
+    document.addEventListener('click', activeContact);
+    window.addEventListener('scroll', stickyHeader);
+
+
+
+})
+
